9601f7ff9573f5c1a13c0231f6c38db8685943bc
Moyowaaaa <moyowa7@icloud.com>
2022-05-30 21:40:58 +0100

    fix: production fix

--- components/ProjectsHeader.js
+++ components/ProjectsHeader.js
@@ -13,7 +13,7 @@ const ProjectsHeader = () => {
         <Link href='/'><p className='hover:border-b-2 border-[blue]'>Teslim Jimoh</p></Link>
 
 
-        <Link href='https://github.com/teslim191' target="_blank" rel="noreferrer"><div className='flex items-center cursor-pointer hover:underline hover:text-[blue]'>See More &nbsp;&nbsp;<FontAwesomeIcon icon={faGithub} width={20} className="hover:text-[blue]" /></div></Link>
+        <Link href='https://github.com/teslim191?tab=repositories' target="_blank" rel="noreferrer"><div className='flex items-center cursor-pointer hover:underline hover:text-[blue]'>See More &nbsp;&nbsp;<FontAwesomeIcon icon={faGithub} width={20} className="hover:text-[blue]" /></div></Link>
         </div>
     </div>
   )
