f826f6fc32cbc3e1d3d156d15110ecc4303fc4c0
Moyowaaaa <moyowa7@icloud.com>
2022-05-30 22:16:28 +0100

    still testing

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,10 +4,10 @@ import Link from 'next/link'
 const Navbar = () => {
   const [navbar, setNavbar] = useState("")
 
-  
+
   return (
     <div>
-        <div className={`${navbar} 'hidden lg:flex items-center w-7/12 h-[5rem] justify-between px-6 cursor-pointer bg-[black] text-[white] fixed z-50'`}>
+        <div className={`hidden lg:flex items-center w-7/12 h-[5rem] justify-between px-6 cursor-pointer bg-[black] text-[white] lg:fixed z-50'`}>
         <Link href='/'><p className='hover:border-b-2 border-[blue]'>Teslim Jimoh</p></Link>
 
 
