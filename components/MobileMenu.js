ac75c812f13e62b4ac4cf766d3e07b0dc7dac345
Moyowaaaa <moyowa7@icloud.com>
2022-06-12 13:51:48 +0100

    fix: overlapping mobile menu

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -13,7 +13,7 @@ const MobileMenu = ({openMenu, closeMenu}) => {
 
 
   return (
-      <div className={`w-full border-2 border-[teal] absolute h-screen overlay ${openMenu ? "hidden":"block"}`}>
+      <div className={`w-full border-2 border-[teal] absolute h-screen overlay z-50 ${openMenu ? "hidden":"block"}`}>
           <div className='flex flex-col h-full justify-center items-center'>
               {/* <div className=' text-3xl text-white text-white w-[55px] flex items-center justify-center bg-[skyblue] py-2 px-2 hover:bg-[blue] text-white cursor-pointer rounded-full 'onClick={closeMenu}>X</div> */}
               <div className='text-3xl text-white bg-[blue] py-2 px-4 rounded-full' onClick={closeMenu}>
