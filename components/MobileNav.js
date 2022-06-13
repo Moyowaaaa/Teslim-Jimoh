f8777c31694f9ec3dee1542f5fc71ea620827f9a
Moyowaaaa <moyowa7@icloud.com>
2022-06-13 16:09:12 +0100

    responsiveness

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -1,46 +1,62 @@
 import React,{useEffect, useState} from 'react'
 import MobileMenu from './MobileMenu'
 
+import Link from 'next/link'
+import Router from 'next/router'
+import { useRouter } from "next/router";
+
 
 
 const MobileNav = () => {
-    const [openMenu, setOpenMenu] = useState(true)
-
-
-
-    const open = () => {
-   setOpenMenu(false)
-    }
+    const [openMenu, setOpenMenu] = useState(false)
+    const router = useRouter();
 
     const closeMenu = () => {
-      setOpenMenu(true)
+      setOpenMenu(false)
     }
-
   
 
+    return (
+
+      <div className='lg:hidden z-50 bg-[black] text-[blue]'>
+        
+        
+      {openMenu ?  <button className="top-6 right-10 fixed z-50 text-4xl " onClick={() => setOpenMenu(!openMenu)}> &#9776;</button>:
+        "" }
 
 
-   
+      <button className="top-6 right-10 fixed z-50 text-4xl" onClick={() => setOpenMenu(!openMenu)}> &#9776;</button>
 
-  return (
-    <div className='flex w-screen  lg:hidden h-[5rem] '>
-
-      <div className='items-center flex w-full justify-between px-4'>
-    <div className='flex flex-col justify-center text-base'>
-        Teslim Jimoh.
-    </div>
-
-
-
-
-
-  <span id="button" className='text-[40px] cursor-pointer' onClick={open}>
-  &#9776;</span>
-  </div>
+      <div className={`"top-0 right-0 fixed text-white bg-[blue] shadow-lg h-full w-screen z-50 ease-in-out duration-700 " ${openMenu ? " translate-x-0" : " translate-x-full"} `}>
+      <button className='top-4 right-4 fixed z-50 text-2xl' onClick={closeMenu}>X</button>
 
 
 
-  <MobileMenu openMenu={openMenu} closeMenu={closeMenu} />
+      <div className='w-full h-4/6'>
+        <div className=' flex flex-col h-full items-center justify-center gap-8' >
+          <Link href='/' >
+            <p onClick={closeMenu}>home</p>
+          </Link>
+
+        <Link href ="/about">
+        <p onClick={closeMenu} className={`hover:border-b-2 border-[blue] ${router.pathname === "/about" || router.pathname == '/about' ? " " : ""}` }>about</p>
+        </Link>
+
+
+        <Link href="/projects">
+             <p onClick={closeMenu} className='hover:border-b-2 border-[blue]'>projects</p>
+        </Link>
+
+        <Link href="/about/#contact">
+                
+                <p onClick={closeMenu} className={`hover:border-b-2 border-[blue] ${router.pathname === "/about#contac" || router.pathname == '/about#contact' ? "border-b-2 border-[blue] " : ""}`}>contact</p></Link>
+
+
+        </div>
+
+        </div>
+     
+        </div>
     </div>
   )
 }
