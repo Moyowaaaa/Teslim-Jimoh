98212a9e911d2648ede4beff2c23d44c2bb48edc
Moyowaaaa <moyowa7@icloud.com>
2022-08-08 11:35:06 +0100

    testing gsap animation

--- components/Teslim.jsx
+++ components/Teslim.jsx
@@ -1,12 +1,17 @@
 
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect, useRef} from 'react'
 import Image from "next/image";
+import gsap from 'gsap';
 import picture from "../images/2.jpg";
 
 const Teslim = () => {
+  let image = useRef(null);
+  console.log(image);
+
+
   return (
-    <div className='h-full  py-[2rem] lg:py-0  w-full flex flex-col justify-center items-center'>
-      <div className='h-4/6 w-full flex flex-col items-center justify-center lg:w-11/12'>
+    <div className='h-full  py-[2rem] lg:py-0  w-full flex flex-col justify-center items-center' ref={el => image = el}>
+      <div className='h-4/6 w-full flex flex-col items-center justify-center lg:w-11/12' >
       <Image src={picture} alt='teslim' className='object-cover h-48 w-96'/>
       </div>
     </div>
