d523811d49ba5c7e3aefc44017bb99e321a14a64
Moyowaaaa <moyowa7@icloud.com>
2022-08-16 13:41:17 +0100

    testing gsap

--- components/Teslim.jsx
+++ components/Teslim.jsx
@@ -1,12 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
+import {gsap} from "gsap"
 
 import picture from "../images/2.jpg";
 
-const Teslim = ({ timeline, ease }) => {
+const Teslim = () => {
+
+  const tl = gsap.timeline()
+
+  useEffect(() => {
+    
+    tl
+    .from(".number", { duration: 2, opacity: 0, y: -300, ease: "Bounce.easeOut" })
+  })
+
+
   return (
     <div className="h-full  py-[2rem] lg:py-0  w-full flex flex-col justify-center items-center">
-      <div className="h-4/6 w-full flex flex-col items-center justify-center lg:w-11/12">
+      <div className="number h-4/6 w-full flex flex-col items-center justify-center lg:w-11/12">
         <Image src={picture} alt="teslim" className="object-cover h-48 w-96" />
       </div>
     </div>
