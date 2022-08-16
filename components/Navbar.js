80b75742bdbc0ec4a306f6d1c5a9aa1223c38273
Moyowaaaa <moyowa7@icloud.com>
2022-08-16 13:59:14 +0100

    testing gsap

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,11 +2,53 @@ import React,{useRef, useState,useEffect} from 'react'
 import Link from 'next/link'
 import Router from 'next/router'
 import { useRouter } from "next/router";
+import {gsap} from "gsap"
 
 
-const Navbar = ({timeline, ease}) => {
+const Navbar = () => {
 
   const router = useRouter();
+
+  const tl = gsap.timeline()
+
+  useEffect(() => {
+    tl.from(".name",{
+      duration:1,
+      delay:0.8,
+      x:-200,
+      ease:"power2.easeOut"
+    })
+
+    tl.from(".links", {
+      stagger:1.5,
+      y:"-200",
+      duration:0.6,
+      delay:0.2,
+      ease:"power3.out"
+    })
+
+    // tl.from(".secondLink", {
+    //   stagger:1.5,
+    //   y:1200,
+    //   duration:0.6,
+    //   delay:0.1,
+    //   ease:"power3.out"
+    // })
+
+
+    // tl.from(".thirdLink", {
+    //   stagger:1.5,
+    //   y:1200,
+    //   duration:0.6,
+    //   delay:0.1,
+    //   ease:"power3.out"
+    // })
+
+
+
+
+
+  })
 
 
 
@@ -15,23 +57,23 @@ const Navbar = ({timeline, ease}) => {
   return (
     <div>
         <div className={`hidden lg:flex items-center w-full h-[4rem] justify-between px-6 cursor-pointer bg-[black] text-[white] lg:fixed z-50 shadow-lg shadow-blue-200" ${router.pathname === "/about" || router.pathname == '/about' ? "w-screen" : ""}`} >
-        <Link href='/'><p className='link' >Teslim Jimoh.</p></Link>
+        <Link href='/'><p className='name link' >Teslim Jimoh.</p></Link>
 
 
-        <div className='flex gap-5 cursor-pointer'>
+        <div className='flex gap-5 cursor-pointer links'>
           <Link href ="/about"  >
-        <p className={`link ${router.pathname === "/about" || router.pathname == '/about' ? " " : ""}` }>about</p>
+        <p className={`link firstLink ${router.pathname === "/about" || router.pathname == '/about' ? " " : ""}` }>about</p>
         </Link>
 
-        <Link href="/projects">
-             <p className='link'>projects</p>
+        <Link href="/projects" >
+             <p className='secondLink link'>projects</p>
         </Link>
 
 
         
-              <Link href="/about/#contact"  >
+              <Link href="/about/#contact" >
                 
-                <p className={`link ${router.pathname === "/about#contac" || router.pathname == '/about#contact' ? "border-b-2 border-[#98dad9] " : ""}`}>contact</p></Link>
+                <p className={`link thirdLink ${router.pathname === "/about#contac" || router.pathname == '/about#contact' ? "border-b-2 border-[#98dad9] " : ""}`}>contact</p></Link>
             </div>
         </div>
     </div>
