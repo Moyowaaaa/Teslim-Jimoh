91f20aa4a91d5f5a9fe9fc6581910982439ddc16
Moyowaaaa <moyowa7@icloud.com>
2022-08-30 15:06:05 +0100

    fix: preloader

--- components/Loading.js
+++ components/Loading.js
@@ -16,40 +16,40 @@ const Loading = () => {
 
   tl.to(".middle",{
       duration:1.9,
-      delay:1.4,
+      delay:.6,
       y:-1400,
       ease:"power3.easeInOut",
       opacity:0
   })
   tl.to(".loading-text", {
     duration:1.9,
-    delay:1.2,
+    delay:.9,
     ease:"power3.easeinOut",
     opacity:0
   })
 
   tl.to(".third",{
       duration:1.7,
-      delay:1.2,
+      delay:.9,
       y:-1600,
       ease:"bounce.out",
       opacity:0
 }) 
 },[])
   return (
-    <div></div>
-    // <div className='overlay absolute w-full h-screen '>
-    //     <div className='flex h-full '>
-    // <div className='first w-4/12 h-full bg-[#46b1a0]'></div>
-    // <div className='middle w-4/12 h-full bg-[#46b1a0]'></div>
-    // <div className='third w-4/12 h-full bg-[#46b1a0]'></div>
+    
+    <div className='overlay absolute w-full h-screen '>
+        <div className='flex h-full '>
+    <div className='first w-4/12 h-full bg-[#46b1a0]'></div>
+    <div className='middle w-4/12 h-full bg-[#46b1a0]'></div>
+    <div className='third w-4/12 h-full bg-[#46b1a0]'></div>
 
-    // <div className='loading-text absolute w-full h-full flex items-center justify-center flex-col'>
+    <div className='loading-text absolute w-full h-full flex items-center justify-center flex-col'>
         
-    //    <h1 className='text-3xl font-bold text-[#112240]'> Teslim Jimoh </h1>
-    //     <h1 className='text-3xl font-bold text-[white]'>Backend Engineer</h1>
+       <h1 className='text-3xl font-bold text-[#112240]'> Teslim Jimoh </h1>
+        <h1 className='text-3xl font-bold text-[white]'>Backend Engineer</h1>
         
-    //     </div>
+        </div>
             
 
 
